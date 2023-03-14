4f13bf976755e85299f78e10e3e7af985c66884e
Abdon França neto <oliabdon006@gmail.com>
2023-03-14 14:23:11 -0300

    primeiro commit

---
+++ src/components/Pedidos.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="main-container">
+        <h1>Gerenciador pedidos:</h1>
+        <Dashboard></Dashboard>
+    </div>
+</template>
+
+<script>
+import Dashboard from './Dashboard.vue';
+export default{
+    name: 'Pedidos',
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <Banner></Banner>
+  <div class="main-container">
+    <h1>Monte o seu burguer</h1>
+    <BurgerForm></BurgerForm>
+  </div>
+</template>
+
+<script>
+import Banner from '@/components/Banner.vue';
+import BurgerForm from '@/components/BurgerForm.vue';
+export default{
+  name:'Home',
+  components:{
+    Banner,
+    BurgerForm
+  }
+}
+</script>
